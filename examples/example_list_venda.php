5856e4532d7ded58e9796c119f3410eaba96d255
Tiago Cosolin Martins <tiago.cosolin@gmail.com>
2020-04-30 15:59:05 -0300

    5

--- examples/example_list_venda.php
+++ examples/example_list_venda.php
@@ -10,8 +10,26 @@ require '../configApp.php';
 //	'appId'  	=> 'XXXXXXXXXXXX',
 //	'secret' 	=> 'XXXXXXXXXXXXXXXXXXXX',
 //));
-
+$meli = new Meli($appId, $secretKey);
+print_r($meli);
 $userId = $meli -> initConnect();
+print_r($userId);
+// If the code was in get parameter we authorize
+$user = $meli->authorize($_GET['code'], $redirectURI);
+
+// Now we create the sessions with the authenticated user
+$_SESSION['access_token'] = $user['body']->access_token;
+$_SESSION['expires_in'] = $user['body']->expires_in;
+$_SESSION['refresh_token'] = $user['body']->refresh_token;
+
+// We can check if the access token in invalid checking the time
+if($_SESSION['expires_in'] + time() + 1 < time()) {
+    try {
+        print_r($meli->refreshAccessToken());
+    } catch (Exception $e) {
+        echo "Exception: ",  $e->getMessage(), "\n";
+    }
+}
 
 // Login or logout url will be needed depending on current user state.
 if ($userId):
