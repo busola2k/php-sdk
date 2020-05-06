5035571e88b0e20ec1256dad03f4a1a340264612
Tiago Cosolin Martins <tiago.cosolin@gmail.com>
2020-05-05 23:30:30 -0300

    Update example_list_venda.php

--- examples/example_list_venda.php
+++ examples/example_list_venda.php
@@ -10,24 +10,38 @@ require '../configApp.php';
 //	'appId'  	=> 'XXXXXXXXXXXX',
 //	'secret' 	=> 'XXXXXXXXXXXXXXXXXXXX',
 //));
+
 $meli = new Meli($appId, $secretKey);
 
-if($_GET['code']) {
-	
-	// If the code was in get parameter we authorize
-	$user = $meli->authorize($_GET['code'], $redirectURI);
-	
-	// Now we create the sessions with the authenticated user
-	$_SESSION['access_token'] = $user['body']->access_token;
-	$_SESSION['expires_in'] = $user['body']->expires_in;
-	$_SESSION['refrsh_token'] = $user['body']->refresh_token;
+if(isset($_GET['code']) || isset($_SESSION['access_token'])) {
 
-	// We can check if the access token in invalid checking the time
-	if($_SESSION['expires_in'] + time() + 1 < time()) {
-		try {
-		    print_r($meli->refreshAccessToken());
-		} catch (Exception $e) {
-		  	echo "Exception: ",  $e->getMessage(), "\n";
+	// If code exist and session is empty
+	if(isset($_GET['code']) && !isset($_SESSION['access_token'])) {
+		// //If the code was in get parameter we authorize
+		try{
+			$user = $meli->authorize($_GET["code"], $redirectURI);
+			
+			// Now we create the sessions with the authenticated user
+			$_SESSION['access_token'] = $user['body']->access_token;
+			$_SESSION['expires_in'] = time() + $user['body']->expires_in;
+			$_SESSION['refresh_token'] = $user['body']->refresh_token;
+		}catch(Exception $e){
+			echo "Exception: ",  $e->getMessage(), "\n";
+		}
+	} else {
+		// We can check if the access token in invalid checking the time
+		if($_SESSION['expires_in'] < time()) {
+			try {
+				// Make the refresh proccess
+				$refresh = $meli->refreshAccessToken();
+
+				// Now we create the sessions with the new parameters
+				$_SESSION['access_token'] = $refresh['body']->access_token;
+				$_SESSION['expires_in'] = time() + $refresh['body']->expires_in;
+				$_SESSION['refresh_token'] = $refresh['body']->refresh_token;
+			} catch (Exception $e) {
+			  	echo "Exception: ",  $e->getMessage(), "\n";
+			}
 		}
 	}
 
@@ -36,9 +50,8 @@ if($_GET['code']) {
 	echo '</pre>';
 
 } else {
-	echo '<a href="' . $meli->getAuthUrl('https://meli2fisec.herokuapp.com/examples/example_list_venda.php') . '">Login using MercadoLibre oAuth 2.0</a>';
+	echo '<a href="' . $meli->getAuthUrl($redirectURI, Meli::$AUTH_URL[$siteId]) . '">Login using MercadoLibre oAuth 2.0</a>';
 }
-
 // Login or logout url will be needed depending on current user state.
 if ($_SESSION['access_token'] ){
 	
