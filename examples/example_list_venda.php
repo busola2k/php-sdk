52cfcda06463cb0bb96364cd54b1cc53228a0591
Tiago Cosolin Martins <tiago.cosolin@gmail.com>
2020-05-05 21:48:36 -0300

    Update example_list_venda.php

--- examples/example_list_venda.php
+++ examples/example_list_venda.php
@@ -12,21 +12,25 @@ require '../configApp.php';
 //));
 $meli = new Meli($appId, $secretKey);
 
-if(isset($_GET['code']) || isset($_SESSION['access_token'])) {
+if($_GET['code']) {
+	
+	// If the code was in get parameter we authorize
+	$user = $meli->authorize($_GET['code'], $redirectURI);
+	
+	// Now we create the sessions with the authenticated user
+	$_SESSION['access_token'] = $user['body']->access_token;
+	$_SESSION['expires_in'] = $user['body']->expires_in;
+	$_SESSION['refrsh_token'] = $user['body']->refresh_token;
 
-	// If code exist and session is empty
-	if(isset($_GET['code']) && !isset($_SESSION['access_token'])) {
-		// //If the code was in get parameter we authorize
-		try{
-			$user = $meli->authorize($_GET["code"], $redirectURI);
-			
-			// Now we create the sessions with the authenticated user
-			$_SESSION['access_token'] = $user['body']->access_token;
-			$_SESSION['expires_in'] = time() + $user['body']->expires_in;
-			$_SESSION['refresh_token'] = $user['body']->refresh_token;
-		}catch(Exception $e){
-			echo "Exception: ",  $e->getMessage(), "\n";
+	// We can check if the access token in invalid checking the time
+	if($_SESSION['expires_in'] + time() + 1 < time()) {
+		try {
+		    print_r($meli->refreshAccessToken());
+		} catch (Exception $e) {
+		  	echo "Exception: ",  $e->getMessage(), "\n";
 		}
+	}
+	
 	} else {
 		// We can check if the access token in invalid checking the time
 		if($_SESSION['expires_in'] < time()) {
